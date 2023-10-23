b34a7a965a31ae9630bff9aea660aeff0ec4573b
AntonioValentin <cesar_anto30@hotmail.com>
2023-10-23 16:11:10 -0600

    Creación del Esquema 'Vacantes' para la Base de Datos

---
+++ models/Vacantes.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise;
+
+const slug = require('slug')
+const shortid = require('shortid')
+
+const vacantesSchema = new mongoose.Schema({
+    titulo:{
+        type:String,
+        required: 'El nombre de la vancante es Obligatorio',
+        trim: true, // - Cortas los espacios en el nombre
+    },
+    empresa:{
+        type:String,
+        trim: true
+    },
+    ubicacion:{
+        type:String,
+        trim:true,
+        required: 'La Ubicación es Obligatoria',
+    },
+    salario: {
+        type: String,
+        default: 0,
+        trim:true
+    },
+    contrato: {
+        type: String,
+        trim:true
+    },
+    descripcion:{
+        type: String.fromCharCode,
+        trim:true
+    },
+    url:{
+        type: String,
+        lowercase: true, // Convierte a minusculas el texto
+    },
+    skills: [String],
+    candidatos: [{
+        nombre: String,
+        email: String,
+        cv: String
+    }]
+})
+
+vacantesSchema.pre('save', function (next) {
+    // Creacion de la URL
+    const url = slug(this.titulo);
+    this.url = `${url}-${shortid.generate()}`
+    next()
+})
+
+module.exports = mongoose.model('Vacante', vacantesSchema)
