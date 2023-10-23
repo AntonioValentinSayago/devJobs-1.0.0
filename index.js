fc121b7810348ad3fe50449c3cfa602e71a13e79
AntonioValentin <cesar_anto30@hotmail.com>
2023-10-23 12:41:35 -0600

    Configuracion del Puerto, y se guardar la session de Mongoose para la conexion

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+const mongose = require('mongoose')
+require('./config/db');
+
 const express = require('express')
 const exphbs = require('express-handlebars')
 const path = require('path')
 const router = require('./routes')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session);
+
+require('dotenv').config( {path: '.env'} )
 
 const app = express();
 
@@ -16,6 +24,16 @@ app.set('view engine', 'handlebars');
 // * Configuración de los archivos estaticos dle proyecto
 app.use(express.static(path.join(__dirname, 'public')))
 
+// ? No perder la conexion a Mongose se guarda la Cookie
+app.use(cookieParser());
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave:false,
+    saveUninitialized:false,
+    store: new MongoStore({ mongooseConnection: mongose.connection })
+}))
+
 app.use('/' , router() )
 
-app.listen(5000)
+app.listen(process.env.PUERTO)
