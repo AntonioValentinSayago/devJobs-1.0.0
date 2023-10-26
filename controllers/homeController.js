6501e3cdd779ebd6655ecc2c3a6277759c982bb9
AntonioValentin <cesar_anto30@hotmail.com>
2023-10-26 13:40:09 -0600

    Correcion del BUG para poder mostrar las vacantes

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,19 @@
-exports.mostrarTrabajos = ( req, res ) => {
+const mongoose = require('mongoose')
+//* Exportacion del Modelo (Vacante)
+const Vacante = mongoose.model('Vacante')
+
+exports.mostrarTrabajos = async ( req, res, next ) => {
+
+    const vacantes = await Vacante.find().lean();
+
+    if(!vacantes) return next();
+
     res.render('home' , {
         nombrePagina: 'devJobs',
         tagline: 'Encuentra y Publica Trabajos para Desarrolladores Web',
         barra: true,
-        boton: true
+        boton: true,
+        vacantes
     })
 }
+
